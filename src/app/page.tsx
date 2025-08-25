b65aa7588a925836ead4168a5e87904257a98e5d
thedhinakarr <codedhinakarr@gmail.com>
2025-08-25 23:03:11 +0200

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to hell. 
         </a>
       </footer>
     </div>
